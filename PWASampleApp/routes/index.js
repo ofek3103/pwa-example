4b8c6423e1af55512b5281575b76490bde430b94
ofek3103 <ofek3103s@gmail.com>
2020-03-17 16:25:15 +0200

    Basic Application Setup

---
+++ PWASampleApp/routes/index.js
@@ -0,0 +1,14 @@
+'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/offline.html', function (req, res) {
+    res.sendFile('public/offline.html');
+});
+
+module.exports = router;
